76e54e03a07b05018bbbfc14c46a7345ec66d8fb
siarut1run <sia.rut1run@gmail.com>
2026-04-28 21:41:28 +0900

    Update epic_service.py

--- services/epic_service.py
+++ services/epic_service.py
@@ -1,2 +1,13 @@
+import aiosqlite
+
+DB_PATH = "data.db"
+
 async def link_account(user, epic_id):
     await user.edit(nick=epic_id)
+
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("""
+        INSERT OR REPLACE INTO users (discord_id, epic_id, current_name)
+        VALUES (?, ?, ?)
+        """, (user.id, epic_id, epic_id))
+        await db.commit()
