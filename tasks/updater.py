ad5f14fe40db12d8aad3769ab47465f82165ff98
siarut1run <sia.rut1run@gmail.com>
2026-04-28 22:18:28 +0900

    Update updater.py

--- tasks/updater.py
+++ tasks/updater.py
@@ -1,22 +1,22 @@
 from discord.ext import tasks
 import aiosqlite
+from services.stat_service import update_user_stats
 
 DB_PATH = "data.db"
 
 def start_tasks(bot):
     update_loop.start(bot)
 
-@tasks.loop(minutes=10)
+@tasks.loop(minutes=30)
 async def update_loop(bot):
+    print("PR自動更新中...")
+
     async with aiosqlite.connect(DB_PATH) as db:
-        async with db.execute("SELECT discord_id, epic_id FROM users") as cursor:
+        async with db.execute("SELECT discord_id FROM users") as cursor:
             users = await cursor.fetchall()
 
-    for user_id, epic_id in users:
-        for guild in bot.guilds:
-            member = guild.get_member(user_id)
-            if member:
-                try:
-                    await member.edit(nick=epic_id)
-                except:
-                    pass
+    for (user_id,) in users:
+        try:
+            await update_user_stats(user_id)
+        except Exception as e:
+            print(f"更新失敗: {e}")
