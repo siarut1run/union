4208a7faffc985309da95eead129de1f7d1eb409
siarut1run <sia.rut1run@gmail.com>
2026-04-28 21:39:39 +0900

    Update db.py

---
+++ datebase/db.py
@@ -0,0 +1,32 @@
+import aiosqlite
+
+DB_PATH = "data.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            discord_id INTEGER PRIMARY KEY,
+            epic_id TEXT,
+            current_name TEXT,
+            pr INTEGER DEFAULT 0,
+            earnings INTEGER DEFAULT 0
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS teams (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS tournaments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            section TEXT
+        )
+        """)
+
+        await db.commit()
