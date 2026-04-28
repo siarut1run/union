40679be7bb756b50d301959dc8d16b86c493fe59
siarut1run <sia.rut1run@gmail.com>
2026-04-28 21:53:22 +0900

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,17 @@
+import aiosqlite
 
+DB_PATH = "data.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            discord_id INTEGER PRIMARY KEY,
+            epic_id TEXT,
+            current_name TEXT,
+            pr INTEGER DEFAULT 0,
+            earnings INTEGER DEFAULT 0
+        )
+        """)
+
+        await db.commit()
